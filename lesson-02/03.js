ff0fa21d34dee3b18dd0f74b441372d3df1dc9a7
sergey <O.l.h@yandex.ru>
2025-08-27 08:31:58 +0300

    first commit

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,16 +1,15 @@
 let grade;
 
-if (score >= 90 && score <= 100) {
+if (score >= 90) {
     grade = 'A';
-} else if (score >= 80 && score <= 89) {
+} else if (score >= 80) {
     grade = 'B';
-} else if (score >= 70 && score <= 79) {
+} else if (score >= 70) {
     grade = 'C';
-} else if (score >= 50 && score <= 69) {
+} else if (score >= 50) {
     grade = 'D';
-} else if (score >= 0 && score <= 49) {
+} else if (score >= 0) {
     grade = 'F';
 } else {
     grade = 'Invalid score';
 }
-
