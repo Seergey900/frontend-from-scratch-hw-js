e58bc293514fa0ae960eae038cadcc628115f7a3
sergey <O.l.h@yandex.ru>
2025-08-27 18:15:58 +0300

    new commit

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,15 +1,15 @@
-let score   // тестовое значение, можно изменять
+const score = Math.floor(Math.random() * 100)  // тестовое значение, можно изменять
 let grade="A"
 
-// if (score >= 90 && score <= 100) {
-//     grade = 'A';
-// } else if (score >= 80 && score <= 89) {
-//     grade = 'B';
-// } else if (score >= 70 && score <= 79) {
-//     grade = 'C';
-// } else if (score >= 50 && score <= 69) {
-//     grade = 'D';
-// } else if (score >= 0 && score <= 49) {
-//     grade = 'F';
-// }
+if (score >= 90 && score <= 100) {
+    grade = 'A';
+} else if (score >= 80 && score <= 89) {
+    grade = 'B';
+} else if (score >= 70 && score <= 79) {
+    grade = 'C';
+} else if (score >= 50 && score <= 69) {
+    grade = 'D';
+} else if (score >= 0 && score <= 49) {
+    grade = 'F';
+}
 console.log(`Grade: ${grade}`);
