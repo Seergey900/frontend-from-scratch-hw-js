2575074bb6f48e7808c15d0a3e8b63de890d1c43
sergey <O.l.h@yandex.ru>
2025-09-09 12:37:24 +0300

    new commit

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -4,19 +4,10 @@ const game = {
     lumber: 100,
   },
   addResource(resource, amount) {
-    // Проверяем, есть ли такой ресурс в объекте
     if (this.resources.hasOwnProperty(resource)) {
-      this.resources[resource] += amount // добавляем количество
+      this.resources[resource] += amount
     } else {
-      console.log("Invalid resource") // если ресурса нет
+      console.log("Invalid resource")
     }
   },
-}
-
-// Примеры использования:
-game.addResource("gold", 50)   // теперь золото 300
-game.addResource("lumber", 25) // теперь древесина 125
-game.addResource("stone", 10)  // "Invalid resource"
-console.log(game.resources)
-
 }
