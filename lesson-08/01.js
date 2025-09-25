8909961be26106c515ed02a57910679c559c4730
sergey <O.l.h@yandex.ru>
2025-09-25 13:26:29 +0300

    new commit

--- lesson-08/01.js
+++ lesson-08/01.js
@@ -1,14 +1,14 @@
-/function capitalizeWords(str) {
-// Разделяем строку на отдельные слова
-const words = str.split(' ');
+function capitalizeWords(str) {
+    // Разделяем строку на отдельные слова
+    const words = str.split(' ');
 
-// Проходим по каждому слову
-for (let i = 0; i < words.length; i++) {
-    const word = words[i];               // берём текущее слово
-    words[i] = word[0].toUpperCase() +  // первая буква в верхний регистр
-        word.slice(1);           // остальная часть слова без изменений
-}
+    // Проходим по каждому слову
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];               // берём текущее слово
+        words[i] = word[0].toUpperCase() +  // первая буква в верхний регистр
+            word.slice(1);           // остальная часть слова без изменений
+    }
 
-// Склеиваем слова обратно в строку через пробел
-return words.join(' ');
+    // Склеиваем слова обратно в строку через пробел
+    return words.join(' ');
 }
